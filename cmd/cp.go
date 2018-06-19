e1d94c4631ffd0e09b06f88814677ee5161ac2a9
Ichinose Shogo <shogo82148@gmail.com>
2018-06-19 16:03:11 +0900

    add cp subcommand

---
+++ cmd/cp.go
@@ -0,0 +1,50 @@
+// Copyright © 2018 Shogo Ichinose
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"github.com/shogo82148/s3cli-mini/cmd/internal/cp"
+	"github.com/spf13/cobra"
+)
+
+// cpCmd represents the cp command
+var cpCmd = &cobra.Command{
+	Use:   "cp",
+	Short: "Copies a local file or S3 object to another location locally or in S3.",
+	Long: `Copies a local file or S3 object to another location locally or in S3.
+cp
+<LocalPath> <S3Uri> or <S3Uri> <LocalPath> or <S3Uri> <S3Uri>`,
+	Run: cp.Run,
+}
+
+func init() {
+	rootCmd.AddCommand(cpCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// cpCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// cpCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ cmd/internal/cp/cp.go
@@ -0,0 +1,10 @@
+package cp
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// Run runs cp command.
+func Run(cmd *cobra.Command, args []string) {
+
+}
