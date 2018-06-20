902384efa7997ca1a915d80b813015eb5e567aae
Ichinose Shogo <shogo82148@gmail.com>
2018-06-20 20:44:27 +0900

    add version command

---
+++ cmd/version.go
@@ -0,0 +1,48 @@
+// Copyright © 2018 Shogo Ichinose
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/spf13/cobra"
+)
+
+// Version is the version of s3cli-mini
+const Version = "0.0.1"
+
+// cersionCmd represents the cp command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "show the version of s3cli-mini.",
+	Long:  `show the version of s3cli-mini`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf(
+			"s3cli-mini version %s built with %s %s %s\n",
+			Version, runtime.Version(), runtime.GOOS, runtime.GOARCH,
+		)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
