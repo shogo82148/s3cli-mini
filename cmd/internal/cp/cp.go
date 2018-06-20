7030e9d524df015d166d7947c5c388908c9f9963
Ichinose Shogo <shogo82148@gmail.com>
2018-06-20 11:45:56 +0900

    support upload

--- cmd/internal/cp/cp.go
+++ cmd/internal/cp/cp.go
@@ -8,10 +8,13 @@ import (
 	"path"
 	"path/filepath"
 	"strings"
+	"sync"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/aws/external"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/s3manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3/s3manager/s3manageriface"
 	"github.com/spf13/cobra"
 )
 
@@ -72,7 +75,48 @@ func (c *client) newReader(s string) (io.ReadCloser, string, error) {
 	return f, filepath.Base(s), err
 }
 
+type uploader struct {
+	io.WriteCloser
+	wg       sync.WaitGroup
+	uploader s3manageriface.UploaderAPI
+	err      error
+}
+
+func (u *uploader) Close() error {
+	err := u.WriteCloser.Close()
+	u.wg.Wait()
+	if u.err != nil {
+		return u.err
+	}
+	return err
+}
+
 func (c *client) newWriter(s, name string) (io.WriteCloser, error) {
+	if strings.HasPrefix(s, "s3://") {
+		u, err := url.Parse(s)
+		if err != nil {
+			return nil, err
+		}
+		if strings.HasSuffix(u.Path, "/") {
+			u.Path += name
+		}
+		r, w := io.Pipe()
+		uploader := &uploader{
+			WriteCloser: w,
+			uploader:    s3manager.NewUploaderWithClient(c.s3),
+		}
+		uploader.wg.Add(1)
+		go func() {
+			defer uploader.wg.Done()
+			_, err := uploader.uploader.Upload(&s3manager.UploadInput{
+				Bucket: aws.String(u.Host),
+				Key:    aws.String(strings.TrimPrefix(u.Path, "/")),
+				Body:   r,
+			})
+			uploader.err = err
+		}()
+		return uploader, nil
+	}
 	if stat, err := os.Stat(s); err == nil {
 		if stat.IsDir() {
 			s = filepath.Join(s, name)
