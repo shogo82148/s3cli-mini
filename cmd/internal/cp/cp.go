20b2e8f88e6d70fe54fbfe444770665e3b9823c7
Ichinose Shogo <shogo82148@gmail.com>
2018-06-20 11:24:36 +0900

    Completion file name.

--- cmd/internal/cp/cp.go
+++ cmd/internal/cp/cp.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net/url"
 	"os"
+	"path"
+	"path/filepath"
 	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -32,13 +34,13 @@ func Run(cmd *cobra.Command, args []string) {
 		s3: s3.New(cfg),
 	}
 
-	src, err := c.newReader(args[0])
+	src, name, err := c.newReader(args[0])
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer src.Close()
 
-	dest, err := c.newWriter(args[1])
+	dest, err := c.newWriter(args[1], name)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -49,11 +51,11 @@ func Run(cmd *cobra.Command, args []string) {
 	}
 }
 
-func (c *client) newReader(s string) (io.ReadCloser, error) {
+func (c *client) newReader(s string) (io.ReadCloser, string, error) {
 	if strings.HasPrefix(s, "s3://") {
 		u, err := url.Parse(s)
 		if err != nil {
-			return nil, err
+			return nil, "", err
 		}
 		req := c.s3.GetObjectRequest(&s3.GetObjectInput{
 			Bucket: aws.String(u.Host),
@@ -61,13 +63,20 @@ func (c *client) newReader(s string) (io.ReadCloser, error) {
 		})
 		resp, err := req.Send()
 		if err != nil {
-			return nil, err
+			return nil, "", err
 		}
-		return resp.Body, nil
+		return resp.Body, path.Base(u.Path), nil
 	}
-	return os.Open(s)
+
+	f, err := os.Open(s)
+	return f, filepath.Base(s), err
 }
 
-func (c *client) newWriter(s string) (io.WriteCloser, error) {
+func (c *client) newWriter(s, name string) (io.WriteCloser, error) {
+	if stat, err := os.Stat(s); err == nil {
+		if stat.IsDir() {
+			s = filepath.Join(s, name)
+		}
+	}
 	return os.OpenFile(s, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
 }
