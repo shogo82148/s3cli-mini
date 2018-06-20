626c7f61c971cf0405c64b28eb9dd00bba88b1ac
Ichinose Shogo <shogo82148@gmail.com>
2018-06-20 10:05:43 +0900

    update aws-sdk to v2

--- cmd/internal/cp/cp.go
+++ cmd/internal/cp/cp.go
@@ -7,9 +7,9 @@ import (
 	"os"
 	"strings"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/spf13/cobra"
 )
 
@@ -24,10 +24,12 @@ func Run(cmd *cobra.Command, args []string) {
 		return
 	}
 
-	config := aws.NewConfig().WithRegion("ap-northeast-1")
-	session := session.Must(session.NewSession(config))
+	cfg, err := external.LoadDefaultAWSConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
 	c := &client{
-		s3: s3.New(session),
+		s3: s3.New(cfg),
 	}
 
 	src, err := c.newReader(args[0])
@@ -53,14 +55,15 @@ func (c *client) newReader(s string) (io.ReadCloser, error) {
 		if err != nil {
 			return nil, err
 		}
-		out, err := c.s3.GetObject(&s3.GetObjectInput{
+		req := c.s3.GetObjectRequest(&s3.GetObjectInput{
 			Bucket: aws.String(u.Host),
-			Key:    aws.String(u.Path),
+			Key:    aws.String(strings.TrimPrefix(u.Path, "/")),
 		})
+		resp, err := req.Send()
 		if err != nil {
 			return nil, err
 		}
-		return out.Body, nil
+		return resp.Body, nil
 	}
 	return os.Open(s)
 }
