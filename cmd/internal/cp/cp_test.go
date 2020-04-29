a75bd7f6dad59e85cd6720206a2a16bf3094ae7f
Ichinose Shogo <shogo82148@gmail.com>
2020-04-30 08:06:04 +0900

    fix multipart upload error

--- cmd/internal/cp/cp_test.go
+++ cmd/internal/cp/cp_test.go
@@ -85,6 +85,73 @@ func TestCP_Upload(t *testing.T) {
 	}
 }
 
+func TestCP_Upload_Multipart(t *testing.T) {
+	testutils.SkipIfUnitTest(t)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	svc, err := config.NewS3Client()
+	if err != nil {
+		t.Fatal(err)
+	}
+	bucketName, err := testutils.CreateTemporaryBucket(ctx, svc)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer testutils.DeleteBucket(context.Background(), svc, bucketName)
+
+	// prepare a test file
+	content := bytes.Repeat([]byte("temporary file's content"), 1024*1024)
+	dir, err := ioutil.TempDir("", "s3cli-mini")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+	filename := filepath.Join(dir, "tmpfile")
+	if err := ioutil.WriteFile(filename, content, 0666); err != nil {
+		t.Fatal(err)
+	}
+
+	// test
+	cmd := &cobra.Command{}
+	Run(cmd, []string{filename, "s3://" + bucketName + "/tmpfile.html"})
+
+	resp, err := svc.GetObjectRequest(&s3.GetObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String("tmpfile.html"),
+	}).Send(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// check body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp.Body.Close()
+	if string(body) != string(content) {
+		t.Errorf("want %s, got %s", string(content), string(body))
+	}
+	if aws.StringValue(resp.ContentType) != "text/html; charset=utf-8" {
+		t.Errorf("unexpected content-type: want %s, got %s", "text/html; charset-utf-8", aws.StringValue(resp.ContentType))
+	}
+
+	// check acl
+	retACL, err := svc.GetObjectAclRequest(&s3.GetObjectAclInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String("tmpfile.html"),
+	}).Send(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, g := range retACL.Grants {
+		if g.Grantee.Type != s3.TypeCanonicalUser {
+			t.Errorf("unexpected grantee type, want %s, got %s", s3.TypeCanonicalUser, g.Grantee.Type)
+		}
+	}
+}
+
 func TestCP_Upload_KeyOmitted(t *testing.T) {
 	testutils.SkipIfUnitTest(t)
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
