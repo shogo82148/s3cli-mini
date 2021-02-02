b0a2f42207ad2225e1a88a69e53d816837dd56ae
Ichinose Shogo <shogo82148@gmail.com>
2021-02-02 09:56:21 +0900

    bump v0.0.7

--- cmd/version.go
+++ cmd/version.go
@@ -28,7 +28,7 @@ import (
 )
 
 // Version is the version of s3cli-mini
-const Version = "0.0.6"
+const Version = "0.0.7"
 
 // versionCmd represents the cp command
 var versionCmd = &cobra.Command{
