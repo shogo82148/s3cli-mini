153a448cf4df5d544b7b099b9ac16aa25e942db3
Ichinose Shogo <shogo82148@gmail.com>
2021-12-04 02:30:57 +0900

    remove unused options

--- cmd/root.go
+++ cmd/root.go
@@ -59,10 +59,6 @@ func init() {
 	// will be global for your application.
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.s3cli-mini.yaml)")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
 	config.InitFlag(rootCmd)
 }
 
