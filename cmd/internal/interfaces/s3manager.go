d4429897259a4bb889effd5614d82c1cda116540
ICHINOSE Shogo <shogo82148@gmail.com>
2026-03-29 15:30:24 +0900

    refactor: remove unused UploaderClient interface from s3manager

--- cmd/internal/interfaces/s3manager.go
+++ cmd/internal/interfaces/s3manager.go
@@ -6,9 +6,6 @@ import (
 	"github.com/aws/aws-sdk-go-v2/feature/s3/transfermanager"
 )
 
-// UploaderClient foobar
-type UploaderClient interface{}
-
 // DownloaderClient foobar
 type DownloaderClient interface {
 	DownloadObject(ctx context.Context, input *transfermanager.DownloadObjectInput, optFns ...func(*transfermanager.Options)) (*transfermanager.DownloadObjectOutput, error)
